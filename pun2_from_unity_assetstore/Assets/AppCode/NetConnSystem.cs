36d21bab5e056276e0aac2bbb1c594b2980599e9
Houdini <learngame@126.com>
2022-08-30 22:31:21 +0800

    ss

---
+++ pun2_from_unity_assetstore/Assets/AppCode/NetConnSystem.cs
@@ -0,0 +1,20 @@
+using Photon.Pun;
+using Photon.Realtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetConnSystem : MonoBehaviourPunCallbacks
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    //public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    //{
+    //    //base.OnRoomListUpdate(roomList);
+
+    //}
+}
